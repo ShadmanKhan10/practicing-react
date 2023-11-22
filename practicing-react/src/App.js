9f005b3fae04e5b6a925c267d5f360eed2c1e807
SHADMAN KHAN <khanshadman7777@gmail.com>
2023-11-22 18:21:33 +0530

    learned about useState

--- practicing-react/src/App.js
+++ practicing-react/src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [increment, setIncrement] = useState(0);
+
+  const handleIncrement = () => {
+    setIncrement(increment + 1);
+  };
+
+  const handleDecrement = () => {
+    setIncrement(increment - 1);
+  };
+
+  const [message, setMessage] = useState("cart is empty");
+
+  const handleMessage = () => {
+    increment >= 1
+      ? setMessage(`cart has ${increment}  items`)
+      : setMessage("cart is empty");
+  };
+
   return (
     <div className="">
-      <h1>App is running smoothly</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <h1>{increment}</h1>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleMessage}>Send Message</button>
+      <h1>{message}</h1>
     </div>
   );
 }
